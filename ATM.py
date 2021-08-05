10248223bb27f0e8b534b70d478e466b2f2a2d45
hemu33662 <nasaramhemanth33662@gmail.com>
2021-08-05 08:09:47 +0530

    Initial commit

---
+++ ATM.py
@@ -0,0 +1,10 @@
+X = int(input('How much you want to withdraw :'))
+Y = float(input('How much balance is in your account :'))
+if (Y > X):
+    if (X % 5==0):
+        Y = Y - (X + 0.5)
+        print('%.2f'% Y)
+    else:
+        print('Invalid \nYour account balance is',Y)
+else:
+    print("insufficient balance \nYour account balance is :",Y)
