6740dc6a79008d5383b45e4bee45ccb9fc8f07a6
hemu33662 <nasaramhemanth33662@gmail.com>
2021-08-08 14:11:51 +0530

    Initial commit

--- sum of first n numbers.py
+++ sum of first n numbers.py
@@ -1,4 +1,11 @@
-x = int(input())
-a = 0
-b = 0
-for i in range(len(x)):
+T = int(input("Enter a number :"))
+numbers = []
+for i in range(0,T):
+    n = int(input())
+    temp = 0
+    for j in range(T):
+        temp = temp + (n%10)
+        n = int(n/10)
+    numbers.append(temp)
+for k in range(T):
+    print(numbers[k])
